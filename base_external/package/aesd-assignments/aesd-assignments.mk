163c2ef9b3efbdc2dd843197d7a750e10f01f557
vannidelprete <giovannidelprete95@gmail.com>
2026-01-11 20:06:40 +0100

    Solution of assignment 5 part 2

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,13 +5,14 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = ae73b15
-AESD_ASSIGNMENTS_SITE = git@github.com:vannidelprete/assignement3-vannidelprete.git
+AESD_ASSIGNMENTS_VERSION = master
+AESD_ASSIGNMENTS_SITE = git@github.com:vannidelprete/assignement5-vannidelprete.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -c $(@D)/server all
 endef
 
 # Install writer, finder and finder-test utilities/scripts
@@ -22,6 +23,10 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+
+	# Install aesdsocket application and startup script
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
 $(eval $(generic-package))
