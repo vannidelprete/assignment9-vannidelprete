12176022ce208d58385654bef61d7ac284593321
vannidelprete <giovannidelprete95@gmail.com>
2026-02-08 00:10:24 +0100

    Add ldd package with rootfs overlay for assignment 7

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LDD_VERSION = latest
+LDD_VERSION = d38495cb5f318624dd15935e966c31eb63004369
 LDD_SITE = git@github.com:vannidelprete/assignment7-vannidelprete.git
 LDD_SITE_METHOD = git
 LDD_MODULE_SUBDIRS = scull misc-modules
