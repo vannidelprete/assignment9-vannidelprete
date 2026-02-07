6cc7fdd105a202bbd19593b415200200da1dd574
vannidelprete <giovannidelprete95@gmail.com>
2026-02-08 00:41:20 +0100

    Complete assignment 7: Add automatic /dev/faulty creation

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -9,5 +9,12 @@ LDD_SITE = git@github.com:vannidelprete/assignment7-vannidelprete.git
 LDD_SITE_METHOD = git
 LDD_MODULE_SUBDIRS = scull misc-modules
 
+define LDD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
+endef
+
 $(eval $(kernel-module))
 $(eval $(generic-package))
