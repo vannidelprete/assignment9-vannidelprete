26cc2559d1983dcef9ee9ab7f5647b64d42f09ea
vannidelprete <giovannidelprete95@gmail.com>
2026-02-19 22:31:23 +0100

    Assignment 8 solution

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,14 +5,15 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = 5bf317c8285373b19d94c7b031b97eb8458986be
-AESD_ASSIGNMENTS_SITE = git@github.com:vannidelprete/assignement5-part2-vannidelprete.git
+AESD_ASSIGNMENTS_VERSION = 76f0821e2db458e8f55fdad8c6533b678ce72e0b
+AESD_ASSIGNMENTS_SITE = git@github.com:vannidelprete/assignment8-vannidelprete.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
+	$(MAKE) -C $(@D)/aesd-char-driver KERNELDIR=$(LINUX_DIR) CROSS_COMPILE=$(TARGET_CROSS) ARCH=$(KERNEL_ARCH) M=$(@D)/aesd-char-driver modules
 endef
 
 # Install writer, finder and finder-test utilities/scripts
@@ -27,6 +28,8 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	# Install aesdsocket application and startup script
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	$(INSTALL) -d 0755 $(TARGET_DIR)/lib/modules/$(BR2_LINUX_KERNEL_VERSION)/extra
+	$(INSTALL) -m 0644 $(@D)/aesd-char-driver/aesdchar.ko $(TARGET_DIR)/lib/modules/$(BR2_LINUX_KERNEL_VERSION)/extra/aesdchar.ko
 endef
 
 $(eval $(generic-package))
